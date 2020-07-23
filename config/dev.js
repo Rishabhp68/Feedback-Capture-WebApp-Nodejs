7685808b586d24b6e0e030bcff35c116c031ea6e
Rishabhp68 <rishabhpalohia68@gmail.com>
2020-07-24 00:03:33 +0530

    added google oauth and keys configuration along with mongo db database

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+// dev key
+module.exports = {
+  googleClientID:
+    "83248206550-h3kemhj03dtpltiu5s6277pmrh1ldgct.apps.googleusercontent.com",
+  googleClientSecret: "eGyvyXOcMMyVZD-v29GMgI8q",
+  mongoURI:
+    "mongodb+srv://Rishabhp68:Rish0612@cluster0.koxpp.mongodb.net/emaily?retryWrites=true&w=majority",
+  cookieKey: "nfkjdsbgkjfbskjbvkjfsbfkjbvskjcdbvkjsbdvkjsb",
+};
--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require("express"); // to import express from common js module since node dosenot suppoert es2015
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./model/User");
+require("./service/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express(); // create express app, in a project we need multiple express apps , set configuration for express in app
 
-// assign the router to express app
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
 
-/*
-1. app is running express server, creates a brand new route handler for app
-2. get is get request
-3. '/' is root route
-4. req is request
-5. res response 
-*/
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000; // Dynamic port binding for Hureku
-app.listen(PORT); //listen to port 5000
+app.listen(PORT);
